29d6bd579642e9c0936c7268794f3b5d27d7bab5
Your Name <yadhukrishna1417@gmail.com>
2026-07-04 16:30:32 +0530

    output added

--- Week-2/Spring-Data-JPA/Quick_Example/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
+++ Week-2/Spring-Data-JPA/Quick_Example/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -1,23 +1,43 @@
 package com.cognizant.ormlearn;
 
+import com.cognizant.ormlearn.model.Country;
 import com.cognizant.ormlearn.service.CountryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OrmLearnApplication implements CommandLineRunner {
+import java.util.List;
 
-    @Autowired
-    private CountryService countryService;
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
 
     public static void main(String[] args) {
-        SpringApplication.run(OrmLearnApplication.class, args);
+
+        ApplicationContext context =
+                SpringApplication.run(OrmLearnApplication.class, args);
+
+        LOGGER.info("Inside main");
+
+        countryService = context.getBean(CountryService.class);
+
+        testGetAllCountries();
     }
 
-    @Override
-    public void run(String... args) {
-        countryService.getAllCountries().forEach(System.out::println);
+    private static void testGetAllCountries() {
+
+        LOGGER.info("Start");
+
+        List<Country> countries = countryService.getAllCountries();
+
+        LOGGER.debug("countries={}", countries);
+
+        LOGGER.info("End");
     }
 }
