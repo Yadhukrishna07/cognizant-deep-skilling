aa76fa30e84830d56d227d48e09b065ae0cce9b8
Your Name <yadhukrishna1417@gmail.com>
2026-07-05 08:13:43 +0530

    week 3 output uploded

---
+++ Week-3/Spring REST using Spring Boot 3/5. JWT-handson/Create authentication service that returns JWT/spring-learn/src/main/java/controller/AuthenticationController.java
@@ -0,0 +1,25 @@
+package com.cognizant.spring_learn.controller;
+
+import com.cognizant.spring_learn.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/authenticate")
+    public Map<String, String> authenticate(Principal principal) {
+
+        String token = jwtUtil.generateToken(principal.getName());
+
+        return Collections.singletonMap("token", token);
+    }
+}
---
+++ Week-3/Spring REST using Spring Boot 3/5. JWT-handson/Create authentication service that returns JWT/spring-learn/src/main/java/util/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.cognizant.spring_learn.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(
+            "ThisIsAVeryStrongSecretKeyForJWTAuthentication123456".getBytes()
+    );
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1200000))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
