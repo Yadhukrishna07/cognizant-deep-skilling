f1ba93a15d06a544ace2658685b50e244109b527
Yadhu Krishna <yadhukrishna1417@gmail.com>
2026-07-08 09:00:57 +0530

    Remove unwanted Week-5 folders

--- Week-5/React/1. ReactJS-HOL/myfirstreact/src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1 style={{ textAlign: 'center' }}>welcome to the first session of React</h1>
-  );
-}
-
-export default App;
--- Week-5/React/2. ReactJS-HOL/studentapp/src/Components/Home.js
+++
@@ -1,7 +0,0 @@
-function Home() {
-  return (
-    <h1>Welcome to the Home page of Student Management Portal</h1>
-  );
-}
-
-export default Home;
